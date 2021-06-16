9e602ce9de935a3071590e6ab58ecb79b204ef20
Морель Дмитрий Алексеевич <DAMorel@sberbank.ru>
2021-06-16 17:54:46 +0300

    add start bot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,8 @@
+import bot.FirstAidKitManagementBot;
+
 public class Main {
+    public static void main(String[] args) {
+        FirstAidKitManagementBot bot = new FirstAidKitManagementBot();
+        bot.start();
+    }
 }
---
+++ src/main/java/bot/FirstAidKitManagementBot.java
@@ -0,0 +1,56 @@
+package bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class FirstAidKitManagementBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "FirstAidKitManagementBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/пес":
+                    sendMsg(message, "Сам псина");
+                    break;
+            }
+        }
+    }
+
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new FirstAidKitManagementBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
